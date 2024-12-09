3b9bfa5c80dbca7d21ce8167465564c725627d27
Steffen Fischer <steffeleffe@gmail.com>
2024-12-09 23:48:12 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,28 @@
 fun main() {
+    data class Line(val left : Int, val right : Int)
+    fun getLine(s: String): Line {
+        val (a,b) = s.split("   ").map { it.toInt() }
+        return Line(a,b)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val lines = input.map { getLine(it) }
+        val sortedLeft = lines.map { it.left }.sorted()
+        val sortedRight = lines.map { it.right }.sorted()
+        return sortedLeft.mapIndexed { index, i -> Math.abs(i - sortedRight[index]) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lines = input.map { getLine(it) }
+        val left = lines.map { it.left }
+        val right = lines.map { it.right }
+        return left.sumOf { leftNumber -> leftNumber * right.count { it == leftNumber } }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    checkResult(part1(testInput),11)
+    checkResult(part2(testInput),31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
